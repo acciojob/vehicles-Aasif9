d9fdfa6a4ef21ff51a7ef238949dd02d69e2eec1
John Doe <asif123ali456@gmail.com>
2023-01-21 17:51:45 +0530

    Solution

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,16 +4,26 @@ class Boat implements WaterVehicle {
     private String name;
     private int capacity;
 
-    public Boat(String name, int capacity) {
+    public Boat(String name, int capacity){
         this.name = name;
         this.capacity = capacity;
     }
 
-    public String getVehicleName() {
-        return name;
+    public void setName(String name){
+        this.name = name;
     }
 
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return this.name;
+    }
+
+    @Override
     public int getVehicleCapacity() {
-        return capacity;
+        return this.capacity;
     }
 }
